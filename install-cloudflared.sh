1e18f45f09d1f24f76e4848a17c3c238c7ef02e1
DeNNiiInc <dennii.inc@gmail.com>
2025-12-27 16:27:40 +1100

    Add cloudflared installation script

---
+++ install-cloudflared.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# install-cloudflared.sh
+
+echo "📦 Installing Cloudflared..."
+
+# Add cloudflare gpg key
+mkdir -p --mode=0755 /usr/share/keyrings
+curl -fsSL https://pkg.cloudflare.com/cloudflare-public-v2.gpg | tee /usr/share/keyrings/cloudflare-public-v2.gpg >/dev/null
+
+# Add this repo to your apt repositories
+echo 'deb [signed-by=/usr/share/keyrings/cloudflare-public-v2.gpg] https://pkg.cloudflare.com/cloudflared any main' | tee /etc/apt/sources.list.d/cloudflared.list
+
+# install cloudflared
+apt-get update && apt-get install -y cloudflared
+
+echo "✅ Cloudflared Installed!"
+cloudflared --version
